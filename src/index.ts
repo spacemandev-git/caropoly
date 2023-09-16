b406c0be2c75c1c20ae50d1f8de5fb94128ed467
spacemandev <devbharel@gmail.com>
2023-09-15 21:40:53 -0500

    hono server running

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
-
+import { register } from "./handlers";
 const app = new Hono();
 
-app.get("/", (c) => c.text("Caropoly is live!"));
+app.get("/", (c) => c.text("Hono!"));
+
+app.post("/register", async (c) => await register(c));
 
 export default app;
